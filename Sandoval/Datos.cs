d41fd993aa30724f298cb0271dae62de1994625b
FranklinOrdoñez <FranklinOrdoez@UIOING003A>
2022-11-16 17:36:35 -0500

    Agregar archivos de proyecto.

---
+++ Sandoval/Datos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sandoval
+{
+    internal class Datos
+    {
+        public int codigo { get; set; }
+        public int nombre { get; set; }
+        public int apellido { get; set; }
+        public int edad { get; set; }
+
+
+
+    }
+}
---
+++ Sandoval/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Sandoval
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string Url = "http://192.168.100.55/RinconManabitaJessy/post.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<Sandoval.Datos> _post;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            get();
+        }
+           public async void get() 
+        
+        {
+            var content = await client.GetStringAsync(Url);
+            List<Sandoval.Datos> posts = JsonConvert.DeserializeObject<List<Sandoval.Datos>>(content);
+            _post = new ObservableCollection<Sandoval.Datos>(posts);
+            MyListView.ItemsSource = _post;
+        }
+
+        private async void btnIngresar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new VentanaIngreso());
+        }
+    }
+    
+}
